2e8601435edc39a925afbe33c844f5f18f5b47f1
Barthélémy Tek <barthtek@gmail.com>
2020-10-16 12:56:21 +0200

    findDifferences

--- src/main/java/Zebra.java
+++ src/main/java/Zebra.java
@@ -10,12 +10,16 @@
  */
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 
 import org.chocosolver.examples.AbstractProblem;
 import org.chocosolver.solver.Model;
 import org.chocosolver.solver.constraints.Constraint;
 import org.chocosolver.solver.exception.ContradictionException;
 import org.chocosolver.solver.variables.IntVar;
+
+import com.ibm.icu.impl.Pair;
 
 /**
  * Simple example which solve Zebra puzzle
@@ -106,52 +110,54 @@ public class Zebra extends AbstractProblem {
     @Override
     public void solve() {
         
-    	System.out.println(model);
-    	try {
-            model.getSolver().propagate();
-            
-            HashMap<Integer, ArrayList<IntVar>> casesEgales = new HashMap<Integer, ArrayList<IntVar>>();
-
-            for(int i =  0; i < attr.length ; i++) {
-        		casesEgales.put(i, new ArrayList<IntVar>());
-            }
-            
-            for(int i =  0; i < attr.length ; i++) {
-            	for(int j =  0 ; j < attr[i].length ; j++) {
-//            		System.out.println(i+","+j+" : " + attr[i][j]);
-            		if (attr[i][j].isInstantiated()) {
-            			casesEgales.get(attr[i][j].getValue()).add(attr[i][j]);
-            		}
-            	}
-            }      
-            
-            for(int i =  0; i < attr.length ; i++) {
-            	ArrayList<IntVar> varEgales = (ArrayList<IntVar>) casesEgales.get(i);
-            	for(int j =  0; j < varEgales.size() ; j++) {
-            		varEgales.get(j).eq(i).post();
-            	}
-            }
-            model.getSolver().propagate();
-            System.out.println(model);
-            print(attr);
-            
-        } catch (ContradictionException e) {
-            e.printStackTrace();
-        }
-
-//        while (model.getSolver().solve()) {
-        int z = zebra.getValue();
-        int n = -1;
-        for (int i = 0; i < SIZE; i++) {
-            if (z == attr[NATIONALITY][i].getValue()) {
-                n = i;
-            }
-        }
-        if (n >= 0) {
-            System.out.printf("%n%-13s%s%s%s%n", "",
-                    "============> The Zebra is owned by the ", sAttr[NATIONALITY][n], " <============");
-        }
-        print(attr);
+//    	System.out.println(model);
+//    	try {
+//            model.getSolver().propagate();
+//            
+//            HashMap<Integer, ArrayList<IntVar>> casesEgales = new HashMap<Integer, ArrayList<IntVar>>();
+//
+//            for(int i =  0; i < SIZE ; i++) {
+//        		casesEgales.put(i, new ArrayList<IntVar>());
+//            }
+//            
+//            for(int i =  0; i < SIZE ; i++) {
+//            	for(int j =  0 ; j < SIZE ; j++) {
+////            		System.out.println(i+","+j+" : " + attr[i][j]);
+//            		if (attr[i][j].isInstantiated()) {
+//            			casesEgales.get(attr[i][j].getValue()).add(attr[i][j]);
+//            		}
+//            	}
+//            }      
+//            
+//            for(int i =  0; i < SIZE ; i++) {
+//            	ArrayList<IntVar> varEgales = (ArrayList<IntVar>) casesEgales.get(i);
+//            	for(int j =  0; j < varEgales.size() ; j++) {
+//            		varEgales.get(j).eq(i).post();
+//            	}
+//            }
+//            model.getSolver().propagate();
+//            System.out.println(model);
+//            print(attr);
+//            
+//        } catch (ContradictionException e) {
+//            e.printStackTrace();
+//        }
+//
+////        while (model.getSolver().solve()) {
+//        int z = zebra.getValue();
+//        int n = -1;
+//        for (int i = 0; i < SIZE; i++) {
+//            if (z == attr[NATIONALITY][i].getValue()) {
+//                n = i;
+//            }
+//        }
+//        if (n >= 0) {
+//            System.out.printf("%n%-13s%s%s%s%n", "",
+//                    "============> The Zebra is owned by the ", sAttr[NATIONALITY][n], " <============");
+//        }
+//        print(attr);
+    	
+    	candidateExplanations();
     }
     private void print(IntVar[][] pos) {
         System.out.printf("%-13s%-13s%-13s%-13s%-13s%-13s%n", "",
@@ -176,23 +182,87 @@ public class Zebra extends AbstractProblem {
     private void candidateExplanations() {
     	try {
     		
-    		IntVar[][] oldAttr = attr.clone();
-    		Constraint[] oldCons =  model.getCstrs().clone();
-    		
+    		IntVar[][] oldAttr = deepCopy(attr);
     		model.getSolver().propagate();
+    		List<IntVar> listDifferences = findDifferences(oldAttr, attr);
+    		
+    		for (IntVar n : listDifferences) {
+    			model.getVa
+    		}
     		
     		
-	    	for(int i =  0; i < attr.length ; i++) {
-	        	for(int j =  0 ; j < attr[i].length ; j++) {
-//	        		if (attr[i][j].()) {
-//	        			
-//	        		}
-	        	}
-	        }
-	    	
-	    	
+//    		print(oldAttr);
+//    		print(attr);
+    		
     	 } catch (ContradictionException e) {
              e.printStackTrace();
          }
+    }
+    
+    private IntVar[][] deepCopy(IntVar[][] attr) {
+    	
+    	IntVar[][] ret = new IntVar[SIZE][SIZE];
+    	
+    	for (int i = 0 ; i < SIZE ; i ++) {
+    		for (int j = 0 ; j < SIZE ; j++) {
+    			
+    			Iterator<Integer> valuesItr = attr[i][j].iterator();
+    			ArrayList<Integer> valuesArrayList = new ArrayList<>();
+    			
+    			while (valuesItr.hasNext()) {
+    				Integer v = valuesItr.next();
+    				valuesArrayList.add(v);
+    			}
+    			
+    			int[] valuesArray = valuesArrayList.stream().mapToInt(v -> v.intValue()).toArray();
+    			
+    			ret[i][j] = model.intVar(attr[i][j].getName(), valuesArray);
+    		}
+    	}
+    	return ret;
+    }
+    
+    private List<Pair<IntVar, Integer>> findDifferences(IntVar[][] oldAttr, IntVar[][] newAttr) {
+    	
+    	List<IntVar> ret = new ArrayList<>();
+    	
+    	for (int i = 0 ; i < SIZE ; i ++) {
+    		for (int j = 0 ; j < SIZE ; j++) {
+    			
+    			Iterator<Integer> oldValuesItr = oldAttr[i][j].iterator();
+    			ArrayList<Integer> oldValuesArrayList = new ArrayList<>();
+    			
+    			while (oldValuesItr.hasNext()) {
+    				Integer v = oldValuesItr.next();
+    				oldValuesArrayList.add(v);
+    			}
+    			
+    			Iterator<Integer> newValuesItr = newAttr[i][j].iterator();
+    			ArrayList<Integer> newValuesArrayList = new ArrayList<>();
+    			
+    			while (newValuesItr.hasNext()) {
+    				Integer v = newValuesItr.next();
+    				newValuesArrayList.add(v);
+    			}
+    			
+    			if (!oldValuesArrayList.equals(newValuesArrayList)) {
+    				ret.add(newAttr[i][j]);
+    			}
+    		}
+    	}
+    	return ret;
+    }
+    
+    private IntVar notN(IntVar n) {
+        String name = "NOT_" + n.getName();
+        List<Integer> values = new ArrayList<>();
+        for (int i = 0; i < SIZE; i++) {
+            if (!n.contains(i)){
+                values.add(i);
+            }
+        }
+        int[] valuesArray = values.stream().mapToInt(v -> v.intValue()).toArray();
+        IntVar notN = model.intVar(name, valuesArray);
+        return notN;
     }
 }
